2b059c32a0a870c79db06532872a5d02093463d4
Minna Wang <minna.wang13@gmail.com>
2018-06-07 16:53:08 -0600

    Add accordion and fix image

--- static/scripts.js
+++ static/scripts.js
@@ -1,33 +1,57 @@
-/* 
+/*
  Created on : Jul 4, 2017, 12:43:10 AM
  Author     : Atta-Ur-Rehman Shah (http://attacomsian.com)
+
+ edited by Minna in certain places
  */
-$(function () {
+$(function() {
 
-    //init wow effects
-    new WOW().init();
+  //init wow effects
+  new WOW().init();
 
-    //scroll menu
-    $(window).scroll(function () {
-        var secondFeature = $('#features').offset().top;
-        var scroll = $(window).scrollTop();
-        if (scroll >= 300) {
-            $('.sticky-navigation').css({"top": '0px'});
-        } else {
-            $('.sticky-navigation').css({"top": '-100px'});
-        }
-        if (scroll >= secondFeature - 200) {
-            $(".mobileScreen").css({'background-position': 'center top'});
-        }
-        return false;
+  //scroll menu
+  $(window).scroll(function() {
+    var secondFeature = $('#features').offset().top;
+    var scroll = $(window).scrollTop();
+    if (scroll >= 300) {
+      $('.sticky-navigation').css({
+        "top": '0px'
+      });
+    } else {
+      $('.sticky-navigation').css({
+        "top": '-100px'
+      });
+    }
+    if (scroll >= secondFeature - 200) {
+      $(".mobileScreen").css({
+        'background-position': 'center top'
+      });
+    }
+    return false;
+  });
+
+  //page scroll
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top - 20
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
+  //accordion - Added by minna
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
     });
-    
-    //page scroll
-    $('a.page-scroll').bind('click', function (event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 20
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  }
 });
