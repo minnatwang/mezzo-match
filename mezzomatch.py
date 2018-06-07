683018647f58d93204f2eaf42884208f66accd2b
Minna Wang <minna.wang13@gmail.com>
2018-06-07 09:38:02 -0600

    Add Flask file
    Following Flask walkthrough from https://www.reddit.com/r/Python/comments/5kwyzi/question_hosting_a_python_script_on_heroku/

---
+++ mezzomatch.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def application():
+    answer = logic("example input")
+    return answer
+
+if __name__ == "__main__":
+    app.run()
